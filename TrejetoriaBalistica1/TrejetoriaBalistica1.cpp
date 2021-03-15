814ed70dbd8e2cae5db9934972d3f8c4735f61e5
KalebeRS <windks@outlook.com>
2021-03-15 18:33:05 -0300

    Add as funções requeridas e modificando o .h

--- TrejetoriaBalistica1/TrejetoriaBalistica1.cpp
+++ TrejetoriaBalistica1/TrejetoriaBalistica1.cpp
@@ -1,18 +1,11 @@
 ﻿#include <iostream>
 #include <cmath>
-
-// definindo os valores fixos gerais de PI e gravidade passados no exercício
-// define é usado para passar argumentos constantes que serão usados pelo
-// programa.
-#define PI 3.141592654
-#define G 9.8
-
+#include "./TrejetoriaBalistica1.h"
 
 int main() {
     // iniciando variaveis 
     double v0, distancia;
     double altura, tempo, theta, seno, angulo_graus;
-
 
     // Entrada dos valores de Velocidade Inicial e Distancia
     std::cout << "Velocidade Inicial (m/s): ";
@@ -21,19 +14,45 @@ int main() {
     std::cout << "Distancia (m): ";
     std::cin >> distancia;
 
-    // Cálculo do ângulo, altura e tempo, fazendo a conversão do ângulo
-    // de radianos para graus.
-    seno = (distancia * G) / (pow(v0, 2));
-    theta = asin(seno) / 2;
-    altura = pow(v0, 2) * pow(sin(theta), 2) / (2 * G);
-    tempo = 2 * v0 * sin(theta) / G;
-    angulo_graus = theta * 180 / PI;
-
-
+    // Chamando as funções
+    theta = calculo_theta(distancia, v0);
+    altura = calculo_altura(theta, v0);
+    tempo = calculo_tempo(theta, v0);
+    angulo_graus = radianos_para_graus(theta);
+    
     // Prints da saída dos resultados
     std::cout << "Altura: " << altura << " m" << std::endl;
-    std::cout << "Ângulo: " << angulo_graus << " graus" << std::endl;
+    std::cout << "Angulo: " << angulo_graus << " graus" << std::endl;
     std::cout << "Tempo: " << tempo << " s" << std::endl;
 
     return 0;
+}
+
+double calculo_theta(double distancia, double v0) {
+    // Funçao para calcular o ângulo theta
+    double seno, theta;
+    seno = (distancia * G) / (pow(v0, 2));
+    theta = asin(seno) / 2;
+    return theta;
+}
+
+double calculo_altura(double theta, double v0) {\
+    // Função para calcular a aultura
+    double altura;
+    altura = pow(v0, 2) * pow(sin(theta), 2) / (2 * G);
+    return altura;
+}
+
+double calculo_tempo(double theta, double v0) {
+    //Função para calcular o tempo
+    double tempo;
+    tempo = 2 * v0 * sin(theta) / G;
+    return tempo;
+}
+
+double radianos_para_graus(double theta) {
+    // Função para transformar o ângulo theta de radianos para graus
+    double angulo_graus;
+    angulo_graus = theta * 180 / PI;
+    return angulo_graus;
 }
--- TrejetoriaBalistica1/TrejetoriaBalistica1.h
+++ TrejetoriaBalistica1/TrejetoriaBalistica1.h
@@ -1,8 +1,18 @@
-﻿// TrejetoriaBalistica1.h: arquivo de inclusão para arquivos de inclusão padrão do sistema,
-// ou arquivos de inclusão específicos a um projeto.
+﻿// Arquivo para inclusao de definiçoes e protótipo de funcao 
 
 #pragma once
 
 #include <iostream>
+
+// definindo os valores fixos gerais de PI e gravidade passados no exercício
+// define é usado para passar argumentos constantes que serão usados pelo
+// programa.
+#define PI 3.141592654
+#define G 9.8
+
+double calculo_theta(double, double);
+double calculo_altura(double, double);
+double calculo_tempo(double, double);
+double radianos_para_graus(double);
 
 // TODO: Referencie os cabeçalhos adicionais de que seu programa precisa aqui.
